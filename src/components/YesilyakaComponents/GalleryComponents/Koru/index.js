76d001050ce9f00a5306c9e625e229e55f0d167f
Kamil Karaçam <kamil_karacam@msn.com>
2021-06-02 02:04:09 +0300

    koru gallery done

--- src/components/YesilyakaComponents/GalleryComponents/Koru/index.js
+++ src/components/YesilyakaComponents/GalleryComponents/Koru/index.js
@@ -4,27 +4,27 @@ import React, { useRef, useState } from "react";
 // Import npm packages
 import { Swiper, SwiperSlide } from "swiper/react";
 import
-    SwiperCore,
+SwiperCore,
 {
     Navigation,
     Thumbs
 }
-from 'swiper/core';
+    from 'swiper/core';
 
 //import images & data
-import {KoruGallery} from "../../../../helper/Koru/KoruData";
+import { KoruGallery } from "../../../../helper/Koru/KoruData";
 
 
 //import style
 import './korugallery.scss';
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "swiper/components/navigation/navigation.min.css";
 import Title from "../../../../atoms/Title";
 
 
 
 // install Swiper modules
-SwiperCore.use([Navigation,Thumbs]);
+SwiperCore.use([Navigation, Thumbs]);
 
 
 const KoruGalleryComponent = () => {
@@ -38,11 +38,11 @@ const KoruGalleryComponent = () => {
 
     return (
         <>
-            <Title title="YEŞİLYAKA KORU" subtitle="GALERİ" data-aos={'zoom-in'} data-aos-duration={'1300'}/>
+            <Title title="YEŞİLYAKA KORU" subtitle="GALERİ" data-aos={'zoom-in'} data-aos-duration={'1300'} />
 
             <Swiper
                 spaceBetween={10}
-                thumbs={{swiper: thumbsSwiper}}
+                thumbs={{ swiper: thumbsSwiper }}
                 data-aos={'fade-down'} data-aos-duration={'1300'}
                 className="gallerySwiper">
                 {/* Outside Button Swiper */}
@@ -97,12 +97,12 @@ const KoruGalleryComponent = () => {
                 {/* Inside Button Swiper */}
                 <SwiperSlide>
                     <Swiper
-                      navigation={true}
-                      className="villaInside"
+                        navigation={true}
+                        className="villaInside"
                     >
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.inside} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.inside} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
@@ -160,6 +160,16 @@ const KoruGalleryComponent = () => {
                                 <img src={KoruGallery.inside12} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="imageCase">
+                                <img src={KoruGallery.inside13} alt={KoruGallery.alt} />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="imageCase">
+                                <img src={KoruGallery.inside14} alt={KoruGallery.alt} />
+                            </div>
+                        </SwiperSlide>
                     </Swiper>
                 </SwiperSlide>
                 {/* Social Button Swiper */}
@@ -170,72 +180,82 @@ const KoruGalleryComponent = () => {
                     >
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social1} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social1} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social2} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social2} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social3} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social3} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social4} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social4} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social5} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social5} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social7} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social7} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social8} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social8} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social9} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social9} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social10} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social10} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social11} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social11} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social12} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social12} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social13} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social13} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="imageCase">
-                                <img src={KoruGallery.social14} alt={KoruGallery.alt}/>
+                                <img src={KoruGallery.social14} alt={KoruGallery.alt} />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="imageCase">
+                                <img src={KoruGallery.social15} alt={KoruGallery.alt} />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="imageCase">
+                                <img src={KoruGallery.social16} alt={KoruGallery.alt} />
                             </div>
                         </SwiperSlide>
                     </Swiper>
@@ -243,15 +263,15 @@ const KoruGalleryComponent = () => {
                 {/* Video Button Swiper */}
                 <SwiperSlide>
                     <iframe width="100%" height="500px" src="https://www.youtube.com/embed/GAyh_5xt4ds"
-                            title="YouTube video player" frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen></iframe>
+                        title="YouTube video player" frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
                 </SwiperSlide>
             </Swiper>
             <Swiper
                 onSwiper={
                     setThumbsSwiper}
-                onClick={(aa)=>{
+                onClick={(aa) => {
                     console.log(aa.clickedIndex);
                 }}
                 spaceBetween={0}
@@ -265,7 +285,7 @@ const KoruGalleryComponent = () => {
                     <Button
                         data-aos={'fade-up'} data-aos-duration={'1300'}
                         className="btn-secondary"
-                        style={{'backgroundColor': `${activeTwo}`,borderRadius:'15px'}}
+                        style={{ 'backgroundColor': `${activeTwo}`, borderRadius: '15px' }}
                         onClick={() => {
                             setActiveOne('')
                             setActiveTwo('black')
@@ -275,18 +295,18 @@ const KoruGalleryComponent = () => {
                     >Villa Dış Mekan</Button>
                 </SwiperSlide>
                 <SwiperSlide
-                  className="thumbSlide"
+                    className="thumbSlide"
                 >
                     <Button
-                      className="btn-secondary"
-                      data-aos={'fade-up'} data-aos-duration={'1300'}
-                      style={{'backgroundColor': `${activeOne}`,borderRadius:'15px'}}
-                      onClick={() => {
-                          setActiveOne('black')
-                          setActiveTwo('')
-                          setActiveThree('')
-                          setActiveFour('')
-                      }}
+                        className="btn-secondary"
+                        data-aos={'fade-up'} data-aos-duration={'1300'}
+                        style={{ 'backgroundColor': `${activeOne}`, borderRadius: '15px' }}
+                        onClick={() => {
+                            setActiveOne('black')
+                            setActiveTwo('')
+                            setActiveThree('')
+                            setActiveFour('')
+                        }}
                     >Villa İç Mekan</Button>
                 </SwiperSlide>
                 <SwiperSlide
@@ -295,7 +315,7 @@ const KoruGalleryComponent = () => {
                     <Button
                         className="btn-secondary"
                         data-aos={'fade-up'} data-aos-duration={'1300'}
-                        style={{'backgroundColor': `${activeThree}`,borderRadius:'15px'}}
+                        style={{ 'backgroundColor': `${activeThree}`, borderRadius: '15px' }}
                         onClick={() => {
                             setActiveOne('')
                             setActiveTwo('')
@@ -310,7 +330,7 @@ const KoruGalleryComponent = () => {
                     <Button
                         className="btn-secondary"
                         data-aos={'fade-up'} data-aos-duration={'1300'}
-                        style={{'backgroundColor': `${activeFour}`,borderRadius:'15px'}}
+                        style={{ 'backgroundColor': `${activeFour}`, borderRadius: '15px' }}
                         onClick={() => {
                             setActiveOne('')
                             setActiveTwo('')
--- src/helper/Koru/KoruData.js
+++ src/helper/Koru/KoruData.js
@@ -18,114 +18,118 @@ import ardic from '../../assets/images/Koru/villa/ardic.png';
 import akasya from '../../assets/images/Koru/villa/akasya.png';
 
 //koru gallery
-import inside from '../../assets/images/Koru/galeri/inside/1.png';
-import inside2 from '../../assets/images/Koru/galeri/inside/2.png';
-import inside3 from '../../assets/images/Koru/galeri/inside/3.png';
-import inside4 from '../../assets/images/Koru/galeri/inside/4.png';
-import inside5 from '../../assets/images/Koru/galeri/inside/5.png';
-import inside6 from '../../assets/images/Koru/galeri/inside/6.png';
-import inside7 from '../../assets/images/Koru/galeri/inside/7.png';
-import inside8 from '../../assets/images/Koru/galeri/inside/8.png';
-import inside9 from '../../assets/images/Koru/galeri/inside/9.png';
-import inside10 from '../../assets/images/Koru/galeri/inside/10.png';
-import inside11 from '../../assets/images/Koru/galeri/inside/11.png';
-import inside12 from '../../assets/images/Koru/galeri/inside/12.png';
+import inside from '../../assets/images/Koru/galeri/inside/1.jpg';
+import inside2 from '../../assets/images/Koru/galeri/inside/2.jpg';
+import inside3 from '../../assets/images/Koru/galeri/inside/3.jpg';
+import inside4 from '../../assets/images/Koru/galeri/inside/4.jpg';
+import inside5 from '../../assets/images/Koru/galeri/inside/5.jpg';
+import inside6 from '../../assets/images/Koru/galeri/inside/6.jpg';
+import inside7 from '../../assets/images/Koru/galeri/inside/7.jpg';
+import inside8 from '../../assets/images/Koru/galeri/inside/8.jpg';
+import inside9 from '../../assets/images/Koru/galeri/inside/9.jpg';
+import inside10 from '../../assets/images/Koru/galeri/inside/10.jpg';
+import inside11 from '../../assets/images/Koru/galeri/inside/11.jpg';
+import inside12 from '../../assets/images/Koru/galeri/inside/12.jpg';
+import inside13 from '../../assets/images/Koru/galeri/inside/13.jpg';
+import inside14 from '../../assets/images/Koru/galeri/inside/14.jpg';
 
 
 
 
-import social from '../../assets/images/Koru/galeri/social.png';
-import social1 from '../../assets/images/Koru/galeri/social/1.png';
-import social2 from '../../assets/images/Koru/galeri/social/2.png';
-import social3 from '../../assets/images/Koru/galeri/social/3.png';
-import social4 from '../../assets/images/Koru/galeri/social/4.png';
-import social5 from '../../assets/images/Koru/galeri/social/5.png';
-import social6 from '../../assets/images/Koru/galeri/social/6.png';
-import social7 from '../../assets/images/Koru/galeri/social/kidsfield1.png';
-import social8 from '../../assets/images/Koru/galeri/social/kidsfield2.png';
-import social9 from '../../assets/images/Koru/galeri/social/golet1.png';
-import social10 from '../../assets/images/Koru/galeri/social/klub1.png';
-import social11 from '../../assets/images/Koru/galeri/social/golet2.png';
-import social12 from '../../assets/images/Koru/galeri/social/klub2.png';
-import social13 from '../../assets/images/Koru/galeri/social/koru.png';
-import social14 from '../../assets/images/Koru/galeri/social/koru2.png';
+import social from '../../assets/images/Koru/galeri/social/1.jpg';
+import social1 from '../../assets/images/Koru/galeri/social/2.jpg';
+import social2 from '../../assets/images/Koru/galeri/social/3.jpg';
+import social3 from '../../assets/images/Koru/galeri/social/4.jpg';
+import social4 from '../../assets/images/Koru/galeri/social/5.jpg';
+import social5 from '../../assets/images/Koru/galeri/social/6.jpg';
+import social6 from '../../assets/images/Koru/galeri/social/7.jpg';
+import social7 from '../../assets/images/Koru/galeri/social/8.jpg';
+import social8 from '../../assets/images/Koru/galeri/social/9.jpg';
+import social9 from '../../assets/images/Koru/galeri/social/10.jpg';
+import social10 from '../../assets/images/Koru/galeri/social/11.jpg';
+import social11 from '../../assets/images/Koru/galeri/social/12.jpg';
+import social12 from '../../assets/images/Koru/galeri/social/13.jpg';
+import social13 from '../../assets/images/Koru/galeri/social/14.jpg';
+import social14 from '../../assets/images/Koru/galeri/social/15.jpg';
+import social15 from '../../assets/images/Koru/galeri/social/16.jpg';
+import social16 from '../../assets/images/Koru/galeri/social/17.jpg';
 
 
-import outside1 from '../../assets/images/Koru/galeri/outside/1.png';
-import outside2 from '../../assets/images/Koru/galeri/outside/2.png';
-import outside3 from '../../assets/images/Koru/galeri/outside/3.png';
-import outside4 from '../../assets/images/Koru/galeri/outside/4.png';
-import outside5 from '../../assets/images/Koru/galeri/outside/5.png';
-import outside6 from '../../assets/images/Koru/galeri/outside/6.png';
-import outside7 from '../../assets/images/Koru/galeri/outside/7.png';
-import outside8 from '../../assets/images/Koru/galeri/outside/8.png';
+import outside1 from '../../assets/images/Koru/galeri/outside/1.jpg';
+import outside2 from '../../assets/images/Koru/galeri/outside/2.jpg';
+import outside3 from '../../assets/images/Koru/galeri/outside/3.jpg';
+import outside4 from '../../assets/images/Koru/galeri/outside/4.jpg';
+import outside5 from '../../assets/images/Koru/galeri/outside/5.jpg';
+import outside6 from '../../assets/images/Koru/galeri/outside/6.jpg';
+import outside7 from '../../assets/images/Koru/galeri/outside/7.jpg';
+import outside8 from '../../assets/images/Koru/galeri/outside/8.jpg';
 
 
 
 export const koruIndex = [
     {
-        img:indexHero,
-        alt:'Koru Yeşilyaka',
+        img: indexHero,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexOne,
-        alt:'Koru Yeşilyaka',
+        img: indexOne,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexTwo,
-        alt:'Koru Yeşilyaka',
+        img: indexTwo,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexThree,
-        alt:'Koru Yeşilyaka',
+        img: indexThree,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexFour,
-        alt:'Koru Yeşilyaka',
+        img: indexFour,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexFive,
-        alt:'Koru Yeşilyaka',
+        img: indexFive,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexSix,
-        alt:'Koru Yeşilyaka',
+        img: indexSix,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexSeven,
-        alt:'Koru Yeşilyaka',
+        img: indexSeven,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexEight,
-        alt:'Koru Yeşilyaka',
+        img: indexEight,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexNine,
-        alt:'Koru Yeşilyaka',
+        img: indexNine,
+        alt: 'Koru Yeşilyaka',
     },
     {
-        img:indexTen,
-        alt:'Koru Yeşilyaka',
+        img: indexTen,
+        alt: 'Koru Yeşilyaka',
     }
 ]
 
 
 export const koruVilla = [
     {
-        img:sedir,
-        alt:'Koru Yeşilyaka Sedir Villaları',
+        img: sedir,
+        alt: 'Koru Yeşilyaka Sedir Villaları',
     },
     {
-        img:sogut,
-        alt:'Yeşilyaka Koru Söğüt Villaları',
+        img: sogut,
+        alt: 'Yeşilyaka Koru Söğüt Villaları',
     },
     {
-        img:ardic,
-        alt:'Yeşilyaka Koru Ardic Villaları',
+        img: ardic,
+        alt: 'Yeşilyaka Koru Ardic Villaları',
     },
     {
-        img:akasya,
-        alt:'Yeşilyaka Koru Akasya Villaları',
+        img: akasya,
+        alt: 'Yeşilyaka Koru Akasya Villaları',
     }
 ]
 
@@ -142,6 +146,8 @@ export const KoruGallery = {
     inside10,
     inside11,
     inside12,
+    inside13,
+    inside14,
     outside1,
     outside2,
     outside3,
@@ -165,7 +171,9 @@ export const KoruGallery = {
     social12,
     social13,
     social14,
-    alt:'Yeşilyaka Su Fotoğraf Galeri',
+    social15,
+    social16,
+    alt: 'Yeşilyaka Su Fotoğraf Galeri',
 }
 
 
